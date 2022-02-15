26d96a43172bfbb9eb690f8c1093b2c5984831d5
nancidama <nanci.damasceno@sumup.com>
2022-02-15 13:11:57 -0300

    Organizando arquivos

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Grid} from'@material-ui/core';
+import './Home.css';
+
+function Home(){
+    return (
+        <>  
+          <Grid container>
+              <Grid item>
+
+              </Grid>
+              <Grid item container>
+                  <Grid item>
+
+                  </Grid>
+                  <Grid item>
+
+                  </Grid>
+
+              </Grid>
+              </Grid>  
+        </>
+    );
+}
+
+export default Home;
