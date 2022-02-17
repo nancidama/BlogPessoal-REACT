bf31b667380437fbdc9bba161772a072e151891b
nancidama <nanci.damasceno@sumup.com>
2022-02-17 15:23:32 -0300

    Atualizações nos componentes, login.

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -3,35 +3,36 @@ import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import {Typography, Box, Grid } from '@material-ui/core';
+import'./Footer.css';
 
 function Footer() {
     return (
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#808080", height: "120px" }}>
+                    <Box className='box1'>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "black" }}>Me Sigam nas Redes Sociais </Typography>
+                            <Typography variant="h5" align="center" gutterBottom className='textos'>Nos sigam nas Redes Sociais </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://www.facebook.com/nanci.vitor/" target="_blank">
-                                <FacebookIcon style={{ fontSize: 60, color: "black" }} />
+                                <FacebookIcon className='redes'/>
                             </a>
                             <a href="https://www.instagram.com/nanci.damasceno/" target="_blank">
-                                <InstagramIcon style={{ fontSize: 60, color: "black" }} />
+                                <InstagramIcon className='redes'/>
                             </a>
                             <a href="https://www.linkedin.com/feed/" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 60, color: "black" }} />
+                                <LinkedInIcon className='redes'/>
                             </a>
                         </Box>
                     </Box>
-                    <Box style={{ backgroundColor: "#808080", height: "60px" }}>
+                    <Box className='box2'>
                         <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "black" }} >© 2022 Copyright:</Typography>
+                            <Typography variant="subtitle2" align="center" gutterBottom className='textos'>Nanci Damasceno:</Typography>
                         </Box>
                         <Box>
                             <a target="_blank" href="">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "black" }} align="center">Nanci Damasceno</Typography>
+                                <Typography variant="subtitle2" gutterBottom className='textos' align="center">© 2022 Copyright:</Typography>
                             </a>
                         </Box>
                     </Box>
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -5,22 +5,23 @@ import './Home.css';
 function Home() {
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#D3D3D3" }}>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+                
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "grey11", fontWeight: "bold" }}>Seja bem Vindo(a)!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "grey11", fontWeight: "bold" }}>Expresse aqui seus Pensamentos e Opiniões!</Typography>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'> Seja bem Vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'> Expresse aqui seus Pensamentos e Opiniões!</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
                         </Box>
-                        <Button variant="outlined" style={{ borderColor: "LightGrey", backgroundColor: "#363636", color: "LightGrey" }}>Ver Postagens</Button>
+                        <Button variant="outlined" className='botao'> Ver Postagens</Button>
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
                     <img src="https://imgur.com/tdY7iJp.png" alt="" width="400px" height="600px" />
                 </Grid>
-                <Grid xs={12} style={{ backgroundColor: "black" }}>
+                <Grid xs={12} className='postagens'>
                 </Grid>
             </Grid>
         </>
